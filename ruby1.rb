01711ac4ac72523f2f479ab3df6dd39c51f2fe40
hirao <shiki.19@icloud.com>
2019-11-30 11:07:46 +0900

    変数の更新

--- ruby1.rb
+++ ruby1.rb
@@ -4,3 +4,48 @@ puts "こんにちは#{name}さん"
 
 age = 33
 puts "#{age}歳です"
+
+puts "Hello Ruby"
+puts "-----------------"
+
+puts 33
+puts 2 + 9
+puts "2+9"
+puts 2 - 2
+puts 2 * 2
+puts 2 / 2
+puts 2 % 2
+
+puts "-----------------"
+
+puts "今日もRubyを" + "勉強します"
+puts "10" + "10"
+
+puts "-----------------"
+
+name = "hirao"
+puts name
+
+puts "-----------------"
+
+text = "プログラミングを学んだら、"
+
+puts text + "世界が広がる"
+
+num = 10
+num1 = 23
+puts num + num1
+
+puts num * num1
+
+puts "----------------"
+
+text = "をマスターしよう"
+
+puts "HTML" + text
+puts "CSS" + text
+puts "Ruby" + text
+
+num = num + 10
+
+puts num
