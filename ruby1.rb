b03723d97163994fa11633e50adc31bf4d6ba93b
hirao <shiki.19@icloud.com>
2019-12-03 14:21:13 +0900

    add_progate_ruby1

--- ruby1.rb
+++ ruby1.rb
@@ -49,3 +49,84 @@ puts "Ruby" + text
 num = num + 10
 
 puts num
+
+num1 *= 10
+
+puts num1
+
+puts "----------------"
+
+puts "#{name}と申します"
+puts "#{num}歳とみせかけて違います"
+
+puts "---------------"
+
+score = 97
+if score > 80
+  puts "よく出来ました"
+end
+
+if score > 99
+  puts "よく出来ました"
+end
+
+puts "---------------"
+
+score = 90
+
+puts score > 80
+puts score < 70
+puts score >= 60
+puts score <= 90
+
+puts "---------------"
+
+puts score == 90
+puts score == 80
+puts score != 90
+puts score != 80
+
+name = "shiki"
+puts name == "shiki"
+puts name != "siki"
+
+score = 100
+if score != 90
+  puts "満点ではありません"
+end
+
+puts "---------------"
+
+score = 70
+if score > 80
+  puts "よく出来ました"
+else
+  puts "頑張りましょう"
+end
+
+puts "---------------"
+
+score = 70
+if score > 80
+  puts "よく出来ました"
+elsif score > 66
+  puts "まずまずです"
+else
+  puts "頑張りましょう"
+end
+
+puts "---------------"
+
+score = 90
+if score >= 95 && score <= 85
+  puts "高得点です。次は満点を目指しましょう"
+else
+  puts "自己管理をし、しっかりと学習しましょう！"
+end
+
+if score >=90 || score <=88
+  puts "高得点です。あと少し！"
+
+end
+
+puts "----------------"
